e5497d26555a417bca614dc034e86eb654c16f01
Simona Cappeddu <simona.cappeddu@gmail.com>
2019-07-27 16:51:35 +0100

    controllers

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,30 @@
 class CocktailsController < ApplicationController
+  #resources :cocktails, only: [:index, :show, :new, :create] do
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    # A user can see the details of a given cocktail, with the dose needed for each ingredient
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+      if @cocktail.save
+        redirect_to cocktail_path(@cocktail)
+      else
+        render :new
+      end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,31 @@
 class DosesController < ApplicationController
+  # resources :doses, only: [:new, :create, :destroy]
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    # we need `restaurant_id` to asssociate review with corresponding restaurant
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+      if @dose.save
+        redirect_to dose_path(@dose)
+      else
+        render :new
+      end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to doses_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
 end
