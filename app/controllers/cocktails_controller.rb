87e95495ce399a77926f54c120b47c9a7f9dc2c0
Simona Cappeddu <simona.cappeddu@gmail.com>
2019-07-30 19:28:24 +0100

    more changes

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,12 +1,13 @@
 class CocktailsController < ApplicationController
-  #resources :cocktails, only: [:index, :show, :new, :create] do
   def index
     @cocktails = Cocktail.all
   end
 
   def show
-    @cocktail = Cocktail.find(params[:id])
     # A user can see the details of a given cocktail, with the dose needed for each ingredient
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+    @review = Review.new
   end
 
   def new
@@ -15,11 +16,11 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
-      if @cocktail.save
-        redirect_to cocktail_path(@cocktail)
-      else
-        render :new
-      end
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'new'
+    end
   end
 
   private
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,31 +1,30 @@
 class DosesController < ApplicationController
-  # resources :doses, only: [:new, :create, :destroy]
-  def new
-    # we need @restaurant in our `simple_form_for`
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose = Dose.new
-  end
+  # def new
+  #   @cocktail = Cocktail.find(params[:cocktail_id])
+  #   @dose = Dose.new
+  # end
 
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
-    # we need `restaurant_id` to asssociate review with corresponding restaurant
-    @dose.cocktail = Cocktail.find(params[:cocktail_id])
-      if @dose.save
-        redirect_to dose_path(@dose)
-      else
-        render :new
-      end
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      @review = Review.new
+      render "cocktails/show"
+    end
   end
 
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to doses_path
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
