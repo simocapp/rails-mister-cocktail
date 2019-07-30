d66fa2bf484aa1303ac24fc1d868ec3d9d6dff8f
Simona Cappeddu <simona.cappeddu@gmail.com>
2019-07-30 21:28:09 +0100

    mr cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+
+puts "done"
+
+Cocktail.destroy_all
+
+puts 'Creating cocktails'
+cocktails = [
+  {name: "Margarita"},
+  {name: "Spritz"},
+  {name: "Mojito"},
+  {name: "Long Island"},
+  {name: "Caipirinha"},
+  {name: "Pina Colada"},
+  ]
+Cocktail.create!(cocktails)
+puts 'Finished!'
